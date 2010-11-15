642f8f267be82c7cd98761d75495c8178a11838f
Tres Seaver tseaver@agendaless.com <Tres Seaver tseaver@agendaless.com@97028c0d-5559-4197-9878-980a030e5970>
2010-11-15 23:11:56 +0000

    Flesh out dummy implementation for testing with suds client.
    git-svn-id: svn+ssh://agendaless.com/home/zeomega/svn/wsrp_prototype/trunk/src/wsrplib@32 97028c0d-5559-4197-9878-980a030e5970

--- wsrplib/_service_description.py
+++ wsrplib/_service_description.py
@@ -1,11 +1,20 @@
 from soaplib.service import DefinitionBase
 from soaplib.service import rpc
 
+from wsrplib._datatypes import LocalizedString
+from wsrplib._datatypes import PortletDescription
 from wsrplib._datatypes import RegistrationContext
 from wsrplib._datatypes import ServiceDescription
 from wsrplib._datatypes import desiredLocales
 from wsrplib._namespaces import WSRP_INTF_NAMESPACE
 
+
+def _localized(name, value):
+    ls = LocalizedString()
+    ls.xmlLang = 'en'
+    ls.value = value
+    ls.resourceName = name
+    return ls
 
 class ServiceDescriptionAPI(DefinitionBase):
     __namespace__ = WSRP_INTF_NAMESPACE
@@ -18,6 +27,13 @@ class ServiceDescriptionAPI(DefinitionBase):
         desired_locales,
         ):
         # See WSRP 1.0 spec. 5.2
+        # Dummy implementation for testing.
+        pd = PortletDescription()
+        pd.portletHandle = '0001'
+        pd.title = _localized('title', 'Some Portlet')
         result = ServiceDescription()
         result.requiresRegistration = False
+        result.requiresInitCookie = 'none'
+        result.locales = ['en-US']
+        result.offeredPortlets = [pd]
         return result
--- wsrplib/client.py
+++ wsrplib/client.py
@@ -3,4 +3,14 @@ from suds.client import Client
 if __name__ == '__main__':
     client = Client('http://localhost:7789/?wsdl')
     sd =  client.service.getServiceDescription(None, [])
-    print sd.requiresRegistration
+    print 'Service Description'
+    print '==================='
+    print 'Requires registration:', sd.requiresRegistration
+    print 'Requires init cookie:', sd.requiresInitCookie
+    print 'Locales:', ', '.join(sd.locales[0])
+    for portlet in sd.offeredPortlets[0]:
+        print
+        print 'Portlet'
+        print '-------'
+        print 'Handle:', portlet.portletHandle
+        print 'Title:', portlet.title.value
